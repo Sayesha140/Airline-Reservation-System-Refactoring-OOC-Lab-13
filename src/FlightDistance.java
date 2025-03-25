d4cef5e01d26edac44f80afe3a041d79409021af
Sayesha140 <enaya871@gmail.com>
2025-03-25 15:53:14 +0600

    removed magic literals calculateDistance method.

--- src/FlightDistance.java
+++ src/FlightDistance.java
@@ -1,29 +1,36 @@
-public  class FlightDistance {
+public class FlightDistance {
 
+    private static final double NAUTICAL_MILE_CONVERSION = 0.8684;
+    private static final double KILOMETER_CONVERSION = 1.609344;
+    private static final double MILES_CONVERSION = 1.1515;
+    private static final double DEGREE_TO_RADIAN_FACTOR = Math.PI / 180.0;
+    private static final double RADIAN_TO_DEGREE_FACTOR = 180.0 / Math.PI;
+    private static final double EARTH_DISTANCE_FACTOR = 60.0;
 
     public String[] calculateDistance(double lat1, double lon1, double lat2, double lon2) {
         double theta = lon1 - lon2;
-        double distance = Math.sin(degreeToRadian(lat1)) * Math.sin(degreeToRadian(lat2)) + Math.cos(degreeToRadian(lat1)) * Math.cos(degreeToRadian(lat2)) * Math.cos(degreeToRadian(theta));
+        double distance = Math.sin(degreeToRadian(lat1)) * Math.sin(degreeToRadian(lat2)) +
+                Math.cos(degreeToRadian(lat1)) * Math.cos(degreeToRadian(lat2)) *
+                        Math.cos(degreeToRadian(theta));
+
         distance = Math.acos(distance);
         distance = radianToDegree(distance);
-        distance = distance * 60 * 1.1515;
-        String[] distanceString = new String[3];
-        distanceString[0] = String.format("%.2f", distance * 0.8684);
-        distanceString[1] = String.format("%.2f", distance * 1.609344);
-        distanceString[2] = Double.toString(Math.round(distance * 100.0) / 100.0);
-        return distanceString;
+        distance = distance * EARTH_DISTANCE_FACTOR * MILES_CONVERSION;
 
+        String[] distanceString = new String[3];
+        distanceString[0] = String.format("%.2f", distance * NAUTICAL_MILE_CONVERSION);
+        distanceString[1] = String.format("%.2f", distance * KILOMETER_CONVERSION);
+        distanceString[2] = String.format("%.2f", distance);
+
+        return distanceString;
     }
 
     private double degreeToRadian(double deg) {
-        return (deg * Math.PI / 180.0);
+        return deg * DEGREE_TO_RADIAN_FACTOR;
     }
 
     private double radianToDegree(double rad) {
-        return (rad * 180.0 / Math.PI);
+        return rad * RADIAN_TO_DEGREE_FACTOR;
     }
-
-
-
-
 }
+
